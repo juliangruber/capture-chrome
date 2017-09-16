074edea008dd0ea630ec274658c64b8b3cbcbe13
Julian Gruber <julian@juliangruber.com>
2017-09-16 16:01:17 +0200

    fix unescaped exec()

--- index.js
+++ index.js
@@ -4,19 +4,21 @@ const readFile = promisify(require('fs').readFile)
 const mkdir = promisify(require('fs').mkdir)
 const tempDir = require('temp-dir')
 const chrome = require('chromium-prebuilt')
+const escape = require('shell-escape')
 
 module.exports = async ({ url, width: width = 1024, height: height = 768 }) => {
   const cwd = `${tempDir}/${Date.now()}${Math.random().toString(16).slice(2)}`
   await mkdir(cwd)
   await exec(
-    `${chrome} ${[
+    escape([
+      chrome,
       '--headless',
       '--disable-gpu',
       '--screenshot',
       `--window-size=${width}x${height}`,
       '--hide-scrollbars',
       url
-    ].join(' ')}`,
+    ]),
     { cwd }
   )
   return readFile(`${cwd}/screenshot.png`)
