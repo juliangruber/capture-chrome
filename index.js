ab97758cf37dcf19399ab5baf9e0c0a43dcea23e
Julian Gruber <julian@juliangruber.com>
2018-08-16 14:06:30 +0200

    run `npm test`

--- index.js
+++ index.js
@@ -4,29 +4,30 @@ const mkdir = promisify(require('fs').mkdir)
 const tempDir = require('temp-dir')
 const puppeteer = require('puppeteer')
 
-module.exports = async ({ 
-  url, 
-  width: width = 1024, 
-  height: height = 768 , 
-  timeout:timeout = 3000
+module.exports = async ({
+  url,
+  width: width = 1024,
+  height: height = 768,
+  timeout: timeout = 3000
 }) => {
-  const cwd = `${tempDir}/${Date.now()}${Math.random().toString(16).slice(2)}`
+  const cwd = `${tempDir}/${Date.now()}${Math.random()
+    .toString(16)
+    .slice(2)}`
 
-  function timeoutHandler(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  };
+  function timeoutHandler (ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
 
   await mkdir(cwd)
-  await (async() => {
-    const browser = await puppeteer.launch({args: ['--no-sandbox']});
-    const page = await browser.newPage();
-    await page.setViewport({width: width, height: height})
-    await page.goto(url, {waitUntil: 'networkidle2'});
+  await (async () => {
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
+    const page = await browser.newPage()
+    await page.setViewport({ width: width, height: height })
+    await page.goto(url, { waitUntil: 'networkidle2' })
     await timeoutHandler(timeout)
-    await page.screenshot({path: `${cwd}/screenshot.png`});
-    browser.close();
-  })();
+    await page.screenshot({ path: `${cwd}/screenshot.png` })
+    browser.close()
+  })()
 
   return readFile(`${cwd}/screenshot.png`)
-
 }
